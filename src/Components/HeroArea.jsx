9c8198f8167a60a9cfbdbce3ee1c94f19ebaefaf
Harshit <2105891@kiit.ac.in>
2022-12-28 12:50:17 +0530

    animations added to left part

--- src/Components/HeroArea.jsx
+++ src/Components/HeroArea.jsx
@@ -2,32 +2,49 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { Outlet } from 'react-router-dom'
 import CaseManagement from './CaseManagement';
-
 import { useNavigate } from 'react-router-dom';
+import { motion } from "framer-motion"
 
 const HeroArea = () => {
-
-    const url = window.location.href;
     const navigate = useNavigate()
+
+    const containerVariants = {
+        initialProp: {
+            opacity: 0,
+            x: '100vw'
+        },
+        finalProp: {
+            opacity: 1,
+            x: 0,
+            transition: {
+                type: 'spring', delay: 0.5
+            }
+        }
+
+    }
 
     return (
         <div className='heroAreaContainer'>
             <div className="leftContainer">
-                <div className="paraLeft">
+                <motion.div className="paraLeft" initial={{ x: -100 }} animate={{ x: 0 }}
+                    transition={{ duration: .9 }}>
 
                     <h3> <ion-icon name="checkmark-circle-outline"></ion-icon> Efficient, intuitive, no-code</h3>
                     <p>console empowers operational teams work and collaborate more effectively and faster </p>
-                </div>
-                <div className="paraLeft">
+                </motion.div>
+                <motion.div className="paraLeft" initial={{ x: -200 }} animate={{ x: 0 }}
+                    transition={{ duration: .9, delay: .1 }} >
                     <h3> <ion-icon name="checkmark-circle-outline"></ion-icon> Endless capabilities, easily configurable</h3>
                     <p>Console enables compliance and fraud teams to be independent and make faster, more reliable decisions</p>
-                </div>
-                <div className="paraLeft">
+                </motion.div>
+                <motion.div className="paraLeft" initial={{ x: -300 }} animate={{ x: 0 }}
+                    transition={{ duration: .9, delay: .2 }}>
                     <h3> <ion-icon name="checkmark-circle-outline"></ion-icon> Get more done</h3>
                     <p>Console users can get 200% increase in productivity with Flagright</p>
 
-                </div>
-                <a href="/" className='learnMore'>Learn More {'>'} </a>
+                </motion.div>
+                <motion.a initial={{ x: -400 }} animate={{ x: 0 }}
+                    transition={{ duration: .9, delay: .2 }} href="/" className='learnMore'>Learn More {'>'} </motion.a>
             </div>
             <div className="rightContainer">
                 <div className="navigatioNtabs">
@@ -39,12 +56,13 @@ const HeroArea = () => {
                     <NavLink to={"Sanction"}> <button > <ion-icon name="person-add-outline"></ion-icon> Sanction</button></NavLink>
 
                 </div>
-                <div className="imgContainer">
+                <div className="imgContainer" >
 
                     <Outlet />
+
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
     )
 }
 
